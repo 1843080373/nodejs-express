fc3ba4009a140508fdbdfd3bea420ea400b9e915
1843080373 <1843080373@qq.com>
2018-08-26 17:57:54 +0800

    微信其余api

--- weixin/routes/oauth.js
+++ weixin/routes/oauth.js
@@ -28,6 +28,28 @@ router.get('/wx_login', function(req,res, next){
       
 });
 
+router.get('/send_tempMsg', function(req,res, next){
+    request.get(
+        {   
+            url:'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+AppID+'&secret='+AppSecret,
+        },
+        function(error, response, body){
+            if(response.statusCode == 200){
+                
+                // 第三步：拉取用户信息(需scope为 snsapi_userinfo)
+                console.log(JSON.parse(body));
+                var data = JSON.parse(body);
+                var access_token = data.access_token;
+                var openid ='o0BJQ5uR7L6QjrwRek8sklsuOXpc';
+                //发送模板消息
+                sendTemplateMsg(res,openid,access_token);
+            }else{
+                console.log(response.statusCode);
+            }
+        }
+    );
+});
+
 
 router.get('/get_wx_access_token', function(req,res, next){
     //console.log("get_wx_access_token")
@@ -47,7 +69,6 @@ router.get('/get_wx_access_token', function(req,res, next){
                 var data = JSON.parse(body);
                 var access_token = data.access_token;
                 var openid = data.openid;
-                
                 request.get(
                     {
                         url:'https://api.weixin.qq.com/sns/userinfo?access_token='+access_token+'&openid='+openid+'&lang=zh_CN',
@@ -58,8 +79,7 @@ router.get('/get_wx_access_token', function(req,res, next){
                             // 第四步：根据获取的用户信息进行对应操作
                             var userinfo = JSON.parse(body);
                             //console.log(JSON.parse(body));
-                            console.log('获取微信信息成功！');
-                            
+                            console.log('获取微信信息成功！'+body);
                             // 小测试，实际应用中，可以由此创建一个帐户
                             res.send("\
                                 <h1>"+userinfo.nickname+" 的个人信息</h1>\
@@ -88,5 +108,62 @@ function url_encode(url){
     url = url.replace(/\%26/g, "&");
     return url;
 }
+
+
+/**
+ * 发送模板消息
+   @param  { res } res 
+ * @param  { string } openid [发送模板消息的接口需要用到openid参数]
+ * @param  { string } access_token [发送模板消息的接口需要用到access_token参数]
+ */
+
+function sendTemplateMsg(res,openid, access_token) {
+
+        var url = `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=`+access_token; //发送模板消息的接口
+
+        var requestData = { //发送模板消息的数据
+            touser: openid,
+            template_id: 'O7n71NjSoc2dB6w4XOEfRl4aGVOxo-nhVu7w8oJhxnE',
+            url: 'http://m.caihang.com',
+            topcolor: '#000000',
+            data: {
+                keyword3: {
+                    value: '大龙虾',
+                    color: "#328392"
+                },
+                keyword1: {
+                    value: 'OD0001',
+                    color: '#328392'
+                },
+                keyword2: {
+                    value: '预定订单',
+                    color: '#328392'
+                },
+                remark: {
+                    value: '请及时确认订单!',
+                    color: '#929232'
+                },
+                first: {
+                    value: '您好，您有一条待确认订单!',
+                    color: '#000000'
+                }
+            }
+        };
+
+        request({
+            url: url,
+            method: 'post',
+            son: true,
+            headers: {
+                     "content-type": "application/json",
+                    },
+            body: JSON.stringify(requestData),
+        }, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body) // 请求成功的处理逻辑
+                res.send(body);
+            }
+        });
+    }
 
 module.exports = router;
--- weixin/routes/wechat.js
+++ weixin/routes/wechat.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 var xml2js = require('xml2js');
-
+var request = require('request');
 /* 微信校验 */
 var token="imook";
+var tulingAPI="f0d99d4e9fc44ec096afdf510c575437";
 /* GET home page. */
 router.get('/wechat/index', function(req, res, next) {
    try{
@@ -46,11 +47,53 @@ router.post('/wechat/index', function(req, res, next) {
         xml2js.parseString(xml,  {explicitArray : false}, function(err, json) {  
                 var backTime = new Date().getTime();  //创建发送时间，整数
 	            if( json.xml.MsgType == 'event' ){  //消息为事件类型
-	                if( json.xml.EventKey == 'clickEvent' ){
-	                    res.send( getXml( json , backTime , '你戳我干啥...' ) )  //回复用户的消息
+	                if( json.xml.Event == 'clickEvent' ){
+	                    res.send( getTxtXml( json , backTime , '你戳我干啥...' ) )  //回复用户的消息
 	                }
+                    if( json.xml.Event == 'subscribe' ){
+                        var respText='感谢您关注偶,这里会给您提供最新的资讯和公告！\n';
+                        respText+='1  我是文字\n';
+                        respText+='2  我是图片\n';
+                        respText+='3  我是多图文\n';
+                        respText+='4  我发送模板消息\n';
+                        res.send( getTxtXml( json , backTime , respText ) )  //回复用户的消息
+                    }
 	            }else if( json.xml.MsgType == 'text' ){  //消息为文字类型
-	                res.send( getXml( json , backTime , `你发"${json.xml.Content}"过来干啥？` ) )  //回复用户的消息
+                    var word = json.xml.Content;
+                    if('1'==word){
+                         res.send( getTxtXml( json , backTime , '这是一段文字' ) )  //回复用户的消息
+                    }
+                    else if('2'==word){
+                         res.send( getImgXml( json , backTime , 'tLIuitkUyO1S-cAf5j_TAoIBiXFY2Ws_3ZH3VX2UG_l2YiOvZneLCignEEyXW9Re' ) )  //回复用户的消息
+                    }
+                    else if('3'==word){
+                         var articles=new Array();
+                         articles[0]={
+                                         "Title":"晚天吹凉风",
+                                         "Description":"点击进入晚天吹凉风博客",
+                                         "PicUrl":"https://ss0.baidu.com/73x1bjeh1BF3odCf/it/u=1163727520,2326205650&fm=85&s=FAD05B8DC2B02637103D65850300B097",
+                                         "Url":"http://www.baidu.com"
+                                    };
+                         articles[1]={
+                                         "Title":"晚天吹凉风",
+                                         "Description":"点击进入晚天吹凉风博客",
+                                         "PicUrl":"https://ss0.baidu.com/73x1bjeh1BF3odCf/it/u=1163727520,2326205650&fm=85&s=FAD05B8DC2B02637103D65850300B097",
+                                         "Url":"http://www.baidu.com"
+                                    };
+                         articles[2]={
+                                         "Title":"晚天吹凉风",
+                                         "Description":"点击进入晚天吹凉风博客",
+                                         "PicUrl":"https://ss0.baidu.com/73x1bjeh1BF3odCf/it/u=1163727520,2326205650&fm=85&s=FAD05B8DC2B02637103D65850300B097",
+                                         "Url":"http://www.baidu.com"
+                                    };
+                         res.send( getNewsXml( json , backTime , articles) )  //回复用户的消息
+                    }
+                    else if('4'==word){
+                         sendTempMsg( res,json,backTime) 
+                    }else{
+                        info_tulingAPI(res,json,backTime);
+                    }
+                    
 	            }
             }); 
         }); 
@@ -59,7 +102,64 @@ router.post('/wechat/index', function(req, res, next) {
   	}
 });
 
-function getXml( json , backTime , word ){
+
+function getImgXml( json , backTime , mediaId ){
+        var backXML = `
+                <xml>
+                    <ToUserName><![CDATA[${json.xml.FromUserName}]]></ToUserName>
+                    <FromUserName><![CDATA[${json.xml.ToUserName}]]></FromUserName>
+                    <CreateTime>${backTime}</CreateTime>
+                    <MsgType>image</MsgType>
+                    <Image>
+                       <MediaId>${mediaId}</MediaId>
+                    </Image>
+                </xml>
+            `
+        return backXML;
+};
+
+function getNewsXml( json , backTime , articles ){
+        var items='';
+        for(var i=0;i<articles.length;i++){
+             var article= articles[i];
+             items+=`<item>
+                      <Title><![CDATA[`+article.Title+`]]></Title>
+                      <Description><![CDATA[`+article.Description+`]]></Description>
+                      <PicUrl><![CDATA[`+article.PicUrl+`]]></PicUrl>
+                      <Url><![CDATA[`+article.Url+`]]></Url>
+                      </item>`;
+        }
+        var backXML = `
+               <xml>
+                      <ToUserName><![CDATA[${json.xml.FromUserName}]]></ToUserName>
+                      <FromUserName><![CDATA[${json.xml.ToUserName}]]></FromUserName>
+                      <CreateTime>${backTime}</CreateTime>
+                      <MsgType><![CDATA[news]]></MsgType>
+                      <ArticleCount>`+articles.length+`</ArticleCount>
+                      <Articles>`+items+`</Articles>
+               </xml>
+            `;
+        return backXML;
+};
+
+function getLinkXml( json , backTime , url, msgId){
+        var backXML = `
+                <xml>
+                    <ToUserName><![CDATA[${json.xml.FromUserName}]]></ToUserName>
+                    <FromUserName><![CDATA[${json.xml.ToUserName}]]></FromUserName>
+                    <CreateTime>${backTime}</CreateTime>
+                    <MsgType>link</MsgType>
+                    <Title><![CDATA[公众平台官网链接]]></Title>
+                    <Description><![CDATA[公众平台官网链接]]></Description>
+                    <Url>${url}</Url>
+                    <MsgId>1234567890123456</MsgId>
+                </xml>
+            `;
+        console.log(backXML)
+        return backXML;
+};
+
+function getTxtXml( json , backTime , word ){
         var backXML = `
                 <xml>
                     <ToUserName><![CDATA[${json.xml.FromUserName}]]></ToUserName>
@@ -70,6 +170,53 @@ function getXml( json , backTime , word ){
                 </xml>
             `
         return backXML;
+};
+
+function info_tulingAPI( res,data,backTime ){
+      var word = data.xml.Content;
+      request.get(
+                    {
+                        url:'http://www.tuling123.com/openapi/api?key='+tulingAPI+'&info='+word,
+                    },
+                    function(error, response, body){
+                        if(response.statusCode == 200){
+                            var back = '';
+                            // 第四步：根据获取的用户信息进行对应操作
+                            var json = JSON.parse(body);
+                            /**
+                             * code 说明 100000文本类 200000链接类 302000新闻类 308000菜谱类
+                             */
+                            var s = "";
+                            if (100000 == json.code) {
+                                s = json.text;
+                                back = back + s;
+                            } else if (200000 == json.code) {
+                                s = json.text;
+                                back = back + s;
+                                back = "\n";
+                                s = json.url;
+                                back = back + s;
+                            } else if (302000 == json.code) {
+                                s = "待开发有点麻烦！\n";
+                                 back = back + s;
+                            } else if (308000 == json.code) {
+                                s = "待开发有点麻烦！\n";
+                                 back = back + s;
+                            }
+                            res.send( getTxtXml( data , backTime , back ) )  //回复用户的消息
+                        }else{
+                            console.log(response.statusCode);
+                            res.send( getTxtXml( data , backTime , '发生异常！！！' ) )  //回复用户的消息
+                        }
+                    }
+                );
+};
+
+/**
+发送模板消息
+**/
+function sendTempMsg( res,data,backTime ){
+     
 };
 
 module.exports = router;
